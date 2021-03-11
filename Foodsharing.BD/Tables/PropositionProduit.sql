3ea8ea27bdbbcc8c477c2da5e7f4cfe9f280f28d
Joanna Filip <joanne.filip@gmail.com>
2021-03-11 18:29:45 +0100

    some changes

---
+++ Foodsharing.BD/Tables/PropositionProduit.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[PropositionProduit] (
+    [IdProposition]   INT           IDENTITY (1, 1) NOT NULL,
+    [IdUtilisateur]   INT           NOT NULL,
+    [IdProduit]       INT           NOT NULL,
+    [DateProposition] DATETIME2 (7) NOT NULL,
+    [DateFin]         DATETIME2 (7) NOT NULL,
+    PRIMARY KEY CLUSTERED ([IdProposition] ASC),
+    CONSTRAINT [FK_PropositionProduit_IdProduit] FOREIGN KEY ([IdProduit]) REFERENCES [dbo].[Produit] ([IdProduit]),
+    CONSTRAINT [FK_PropositionProduit_IdUtilisateur] FOREIGN KEY ([IdUtilisateur]) REFERENCES [dbo].[Utilisateur] ([IdUtilisateur])
+);
+
