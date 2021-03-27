8217e4ea20850edc97b69330a6182046a69ab568
Joanna Filip <joanne.filip@gmail.com>
2021-03-27 13:20:31 +0100

    avancement widget

--- Foodsharing.BD/Tables/PropositionProduit.sql
+++ Foodsharing.BD/Tables/PropositionProduit.sql
@@ -3,7 +3,7 @@
     [IdUtilisateur]   INT           NOT NULL,
     [IdProduit]       INT           NOT NULL,
     [DateProposition] DATETIME2 (7) NOT NULL,
-    [DateFin]         DATETIME2 (7) NOT NULL,
+    [DateFin]         DATETIME2 (7) NULL,
     PRIMARY KEY CLUSTERED ([IdProposition] ASC),
     CONSTRAINT [FK_PropositionProduit_IdProduit] FOREIGN KEY ([IdProduit]) REFERENCES [dbo].[Produit] ([IdProduit]),
     CONSTRAINT [FK_PropositionProduit_IdUtilisateur] FOREIGN KEY ([IdUtilisateur]) REFERENCES [dbo].[Utilisateur] ([IdUtilisateur])
--- Foodsharing/Models/ProductViewModel.cs
+++ Foodsharing/Models/ProductViewModel.cs
@@ -61,28 +61,28 @@ namespace Foodsharing.Models
             TitlePost.Title = "Recent Posts";
             //Recent Posts
             RecentPost = new List<Widget>();
-            RecentPost.Add(new Widget() { Image = "garden_sq_09.jpg", UnderTitle = "Beautiful buildings you need to before dying", Date = new DateTime(2020, 1, 1) });
-            RecentPost.Add(new Widget() {Image = "garden_sq_06.jpg", UnderTitle = "Let's make an introduction for creative life", Date = new DateTime(2020, 1, 2) });
-            RecentPost.Add(new Widget() { Image = "garden_sq_02.jpg", UnderTitle = "Did you see the most beautiful sea in the world?", Date = new DateTime(2020, 1, 3) });
+            RecentPost.Add(new Widget() { Image = "a1.jpg", UnderTitle = "The Problem of Food Waste", Date = new DateTime(2021, 3, 1) });
+            RecentPost.Add(new Widget() {Image = "a2.jpg", UnderTitle = "People waste more food than they think", Date = new DateTime(2021, 2, 2) });
+            RecentPost.Add(new Widget() { Image = "a3.jpg", UnderTitle = "Reducing Wasted Food At Home", Date = new DateTime(2021, 1, 30) });
 
             //Advertising
             Advertising = new Widget();
-            Advertising.Image = "banner_04.jpg";
+            Advertising.Image = "ad.jpg";
             Advertising.Title = "Advertising";
 
             //Instagram
             Instagram = new Widget();
             Instagram.Title = "Instagram Feed";
             InstagramPhoto = new List<Widget>();
-            InstagramPhoto.Add(new Widget() { Image = "garden_sq_01.jpg" });
-            InstagramPhoto.Add(new Widget() { Image = "garden_sq_02.jpg" });
-            InstagramPhoto.Add(new Widget() { Image = "garden_sq_03.jpg" });
-            InstagramPhoto.Add(new Widget() { Image = "garden_sq_04.jpg" });
-            InstagramPhoto.Add(new Widget() { Image = "garden_sq_05.jpg" });
-            InstagramPhoto.Add(new Widget() { Image = "garden_sq_06.jpg" });
-            InstagramPhoto.Add(new Widget() { Image = "garden_sq_07.jpg" });
-            InstagramPhoto.Add(new Widget() { Image = "garden_sq_08.jpg" });
-            InstagramPhoto.Add(new Widget() { Image = "garden_sq_09.jpg" });
+            InstagramPhoto.Add(new Widget() { Image = "i1.jpg" });
+            InstagramPhoto.Add(new Widget() { Image = "i2.jpg" });
+            InstagramPhoto.Add(new Widget() { Image = "i3.jpg" });
+            InstagramPhoto.Add(new Widget() { Image = "i4.jpg" });
+            InstagramPhoto.Add(new Widget() { Image = "i5.jpg" });
+            InstagramPhoto.Add(new Widget() { Image = "i6.jpg" });
+            InstagramPhoto.Add(new Widget() { Image = "i7.jpg" });
+            InstagramPhoto.Add(new Widget() { Image = "i8.jpg" });
+            InstagramPhoto.Add(new Widget() { Image = "i9.jpg" });
 
             //Popular Categories
             PopularCategorie = ctx.GetAllTypes();
