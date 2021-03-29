ef8dd10c12f7ef7a9c98aeef457594fc6e97d007
Joanna Filip <joanne.filip@gmail.com>
2021-03-29 18:46:25 +0200

    projet finit

--- Foodsharing/Models/HomeViewModel.cs
+++ Foodsharing/Models/HomeViewModel.cs
@@ -13,6 +13,11 @@ namespace Foodsharing.Models
         private List<MainImageContent> _mainImage;
         private List<ProductContent> _topProduct;
         private List<ContactModel> _contactText;
+        private Widget _instagram;
+        private List<Widget> _instagramPhoto;
+        private Widget _advertising;
+        private List<Widget> _recentPost;
+        private Widget _titlePost;
         public HomeViewModel ()
         {
             //trois images
@@ -30,6 +35,28 @@ namespace Foodsharing.Models
             ContactText.Add(new ContactModel() { Title = "How we help?", Text = "Every year, a third of global food production goes to waste according to studies done by the United Nations Food and Agricultural Organization (FAO). In the spirit of “think global, act local”, the grassroots initiative Foodsharing was founded in Germany in 2012 to tackle the problem of food waste."});
             ContactText.Add(new ContactModel() { Title = "The beginning", Text = "The beginnings of the initiative can be traced to two German cities, Cologne and Berlin, in 2012. In Cologne the online - platform “foodsharing.de” was invented to allow private individuals to share still edible food which they would otherwise throw away"});
 
+            TitlePost = new Widget();
+            TitlePost.Title = "Recent Posts";
+            //Recent Posts
+            RecentPost = new List<Widget>();
+            RecentPost.Add(new Widget() { Image = "a1.jpg", UnderTitle = "The Problem of Food Waste", Date = new DateTime(2021, 3, 1) });
+            RecentPost.Add(new Widget() { Image = "a2.jpg", UnderTitle = "People waste more food than they think", Date = new DateTime(2021, 2, 2) });
+            RecentPost.Add(new Widget() { Image = "a3.jpg", UnderTitle = "Reducing Wasted Food At Home", Date = new DateTime(2021, 1, 30) });
+
+            //Advertising
+            Advertising = new Widget();
+            Advertising.Image = "ad.jpg";
+            Advertising.Title = "Advertising";
+
+            //Instagram
+            Instagram = new Widget();
+            Instagram.Title = "Instagram Feed";
+            InstagramPhoto = new List<Widget>();
+
+            for (int i = 1; i < 10; i++)
+            {
+                InstagramPhoto.Add(new Widget() { Image = "i" + i + ".jpg" });
+            }
 
         }
 
@@ -69,6 +96,65 @@ namespace Foodsharing.Models
                 _contactText = value;
             }
         }
+        public Widget Instagram
+        {
+            get
+            {
+                return _instagram;
+            }
 
+            set
+            {
+                _instagram = value;
+            }
+        }
+        public List<Widget> InstagramPhoto
+        {
+            get
+            {
+                return _instagramPhoto;
+            }
+
+            set
+            {
+                _instagramPhoto = value;
+            }
+        }
+        public Widget Advertising
+        {
+            get
+            {
+                return _advertising;
+            }
+
+            set
+            {
+                _advertising = value;
+            }
+        }
+        public List<Widget> RecentPost
+        {
+            get
+            {
+                return _recentPost;
+            }
+
+            set
+            {
+                _recentPost = value;
+            }
+        }
+        public Widget TitlePost
+        {
+            get
+            {
+                return _titlePost;
+            }
+
+            set
+            {
+                _titlePost = value;
+            }
+        }
     }
 }
